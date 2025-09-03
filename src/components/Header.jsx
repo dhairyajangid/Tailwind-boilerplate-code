2d10c61db466aeb984b7e320b5350d5683058ce2
dhairyajangid <jangiddhairya@gmail.com>
2025-09-03 18:04:53 +0530

    ...change in StoryAI

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { CiSearch } from "react-icons/ci";
+export function Header(){
+   
+   return <div className="flex border border-green-950 h-18 w-full bg-green-900">
+
+       <div className="flex items-center p-5 ml-10 text-md font-medium">StoryAI</div>
+
+        {/* <div className="w-60% justfiy-end border-2 border-blue-300">
+             
+            <div className="flex items-center border rounded-lg p-2 w-38">
+                <input 
+                    className="bg-transparent flex-1 outline-none" 
+                    type="text" 
+                    placeholder="Search Here" 
+                />
+                <CiSearch className="text-gray-500" />
+            </div>
+
+            <div className="border-2 border-blue-300">
+                <button className="w-11 h-5 bg-green-700 border-2 border-green-400 cursor-pointer">
+                 Login
+                </button>
+            </div>
+       </div> */}
+    </div>
+}
---
+++ src/components/inputBox.jsx
@@ -0,0 +1,10 @@
+export function InputBox(){
+    return <div className="flex justify-center">
+        <input className="h-4 w-23 border-2 border-blue-200 "        
+        type="text" placeholder="title" /> <br />
+        <input className="h-4 w-23 border-2 border-blue-200" 
+        type="text" placeholder="description" />
+    </div>
+}
+
+
